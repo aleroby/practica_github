ae90db0729da28768a016684d907346a1bbdfe1c
thadek <gabipamic@gmail.com>
2021-04-22 21:15:45 -0300

    Iniciando proyecto java sobre branch gabi

---
+++ BranchPruebas/src/branchpruebas/Producto.java
@@ -0,0 +1,58 @@
+package branchpruebas;
+
+/**
+ *
+ * @author gpamic
+ */
+public class Producto {
+    private int IDProducto;
+    private String nombre;
+    private Double precio;
+    private String proveedor;
+    private int cantidad;
+
+    public Producto() {
+    }
+
+    public int getIDProducto() {
+        return IDProducto;
+    }
+
+    public void setIDProducto(int IDProducto) {
+        this.IDProducto = IDProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+}
